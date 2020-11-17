a1c4b165af6cd6581a73aa4a654b7a2d8c5428a3
jmestrelozano <jmestrelozano@gmail.com>
2020-11-17 00:28:46 -0500

    cambios en los archivos icons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { WeatherSiteScreen } from './views/WeatherSiteScreen';
-import "./icons/weather-icons-master/css/weather-icons.css"
+import "./icons/weather-icons-master/weather-icons-master/css/weather-icons.css"
 
 
 ReactDOM.render(
